638fba8128234fd4b1fc57f7267f5705c389767a
SkyrusRB <skyrusrb@gmaill.com>
2022-01-13 21:46:55 +0800

    Adding Method ListAllMovies

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Cli.Models;
 
 namespace Cli
 {
@@ -7,6 +10,14 @@ namespace Cli
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public void ListAllMovies(List<Movie> movies)
+        {
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie);
+            }
         }
     }
 }

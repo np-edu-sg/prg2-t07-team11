a4e4f11625a48472c6f509cc3924365b667af748
SkyrusRB <skyrusrb@gmaill.com>
2022-01-13 16:52:13 +0800

    Added Constructors for Cinema Class

--- Cli/Models/Cinema.cs
+++ Cli/Models/Cinema.cs
@@ -2,6 +2,19 @@
 {
     public class Cinema
     {
+        public string Name { get; set; }
+        public int HallNo { get; set; }
+        public int Capacity { get; set; }
 
+        public Cinema()
+        {
+        }
+
+        public Cinema(string name, int hallNo, int capacity)
+        {
+            Name = name;
+            HallNo = hallNo;
+            Capacity = capacity;
+        }
     }
 }

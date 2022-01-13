b3ad84dc1957fc7b4ca81cb9d6d8081438b79b11
Qin Guan <qinguan20040914@gmail.com>
2022-01-13 17:01:51 +0800

    Implemented Order and Screening

--- Cli/Models/Order.cs
+++ Cli/Models/Order.cs
@@ -1,8 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Cli.Models
 {
     public class Order
     {
+        public int OrderNo { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public double Amount { get; set; }
+        public string Status { get; set; }
+        public List<Ticket> TicketList { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(int orderNo, DateTime orderDateTime)
+        {
+            OrderNo = orderNo;
+            OrderDateTime = orderDateTime;
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            TicketList.Add(ticket);
+        }
+
+        public List<Ticket> GetTicketList()
+        {
+            return TicketList;
+        }
     }
 }
--- Cli/Models/Screening.cs
+++ Cli/Models/Screening.cs
@@ -1,6 +1,29 @@
-﻿namespace Cli.Models
+﻿using System;
+
+namespace Cli.Models
 {
     public class Screening
     {
+        public int ScreeningNo { get; set; }
+        public DateTime ScreeningDateTime { get; set; }
+        public string ScreeningType { get; set; }
+        public int SeatsRemaining { get; set; }
+        public Cinema Cinema { get; set; }
+        public Movie Movie { get; set; }
+
+        public Screening()
+        {
+        }
+
+        public Screening(int screeningNo, DateTime screeningDateTime, string screeningType, int seatsRemaining,
+            Cinema cinema, Movie movie)
+        {
+            ScreeningNo = screeningNo;
+            ScreeningDateTime = screeningDateTime;
+            ScreeningType = screeningType;
+            SeatsRemaining = seatsRemaining;
+            Cinema = cinema;
+            Movie = movie;
+        }
     }
 }

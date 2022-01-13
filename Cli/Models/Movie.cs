78778d71c1dbecadf1db8e23aa89227eef0e89cb
SkyrusRB <skyrusrb@gmaill.com>
2022-01-13 17:09:57 +0800

    Added Constructors and Methods for Movie Class

--- Cli/Models/Movie.cs
+++ Cli/Models/Movie.cs
@@ -1,8 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Cli.Models
 {
     public class Movie
     {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string Classification { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public List<string> GenreList { get; set; }
+        public List<string> ScreeningList { get; set; }
+        
+        public Movie(){}
+
+        public Movie(string title, int duration, string classification, DateTime openingDate, List<string> screeningList)
+        {
+            Title = title;
+            Duration = duration;
+            Classification = classification;
+            OpeningDate = openingDate;
+            ScreeningList = screeningList;
+        }
+
+        public List<string> GetGenreList()
+        {
+            return GenreList;
+        }
     }
 }

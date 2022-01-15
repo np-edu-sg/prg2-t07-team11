cc9f57302208d6d79efff71bdda3c16a593184dc
SkyrusRB <skyrusrb@gmaill.com>
2022-01-15 14:24:23 +0800

    Adding Cinema.cs in Repository

---
+++ Core/Repository/Csv/Cinema.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Core.Repository.Csv
+{
+    public class Cinema : ICinema
+    {
+        private readonly List<Core.Models.Cinema> _cinemas = new();
+
+        public Cinema(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path)) throw new Exception("Bad path");
+
+            try
+            {
+                string[] csvLines = File.ReadAllLines(path);
+                foreach (var line in csvLines[1..])
+                {
+                    string[] data = line.Split(",");
+                    _cinemas.Add(new Core.Models.Cinema(
+                        Convert.ToString(data[0]),
+                        Convert.ToInt32(data[1]),
+                        Convert.ToInt32(data[2])));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to read file", ex);
+            }
+        }
+
+        public List<Models.Cinema> Find()
+        {
+            return _cinemas;
+        }
+    }
+}

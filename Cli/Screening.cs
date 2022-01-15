3bcc178fdff4103d716d61185aa74eefd28d5be5
SkyrusRB <skyrusrb@gmaill.com>
2022-01-16 00:52:18 +0800

    Added Screening Class for displaying data in CLI

---
+++ Cli/Screening.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Cli.Display;
+using Core.Repository;
+
+namespace Cli
+{
+    public class Screening
+    {
+        private readonly IScreening _screening;
+        private readonly IDisplay _display;
+
+        public Screening(IScreening screening, IDisplay display)
+        {
+            _screening = screening;
+            _display = display;
+        }
+
+        public void ListAllScreenings()
+        {
+            foreach (var screening in _screening.Find()) _display.Text(screening);
+        }
+    }
+}

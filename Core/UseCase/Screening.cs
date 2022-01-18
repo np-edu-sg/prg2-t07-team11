d36d02a4289a94d82e48e0001eb18bf6b37249ef
SkyrusRB <skyrusrb@gmaill.com>
2022-01-18 20:44:07 +0800

    Added Add Method for Screening class in UseCase

--- Core/UseCase/Screening.cs
+++ Core/UseCase/Screening.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Core.Repository;
 
 namespace Core.UseCase
@@ -6,10 +8,13 @@ namespace Core.UseCase
     public class Screening
     {
         private readonly IScreening _screeningRepository;
-
-        public Screening(IScreening screeningRepository)
+        private readonly ICinema _cinemaRepository;
+        private readonly IMovie _movieRepository;
+        public Screening(IScreening screeningRepository, ICinema cinemaRepository, IMovie movieRepository)
         {
             _screeningRepository = screeningRepository;
+            _cinemaRepository = cinemaRepository;
+            _movieRepository = movieRepository;
         }
 
         public void LoadData() => _screeningRepository.Init();
@@ -17,6 +22,33 @@ namespace Core.UseCase
         public List<Models.Screening> Find()
         {
             return _screeningRepository.Find();
+        }
+
+        public void Add(DateTime dateTime, string screeningType, string cinemaName, int cinemaHallNo, string movieTitle)
+        {
+            var cinema = _cinemaRepository.FindOneByNameAndHallNo(cinemaName, cinemaHallNo);
+            var movie = _movieRepository.FindOneByTitle(movieTitle);
+            var screening = _screeningRepository.FindByCinema(cinema);
+            if (cinema is null) throw new Exception("Invalid cinema");
+            
+            if (dateTime <= movie.OpeningDate)
+            {
+                throw new Exception("Screening DateTime is before Movie OpeningDate");
+            }
+
+            if (screeningType != "2D" && screeningType != "3D")
+            {
+                throw new Exception("ScreeningType must be 2D or 3D");
+            }
+
+            foreach (var s in screening)
+            {
+                if (s.ScreeningDateTime == dateTime)
+                {
+                    throw new Exception("Cinema Hall Is Not Available");
+                }
+            }
+            _screeningRepository.Add(new Models.Screening(_screeningRepository.Find().Count + 1, dateTime, screeningType, cinema, movie));
         }
     }
 }

40497c32c9bda9c2bbfc2bba240a8e82d200a23d
SkyrusRB <skyrusrb@gmaill.com>
2022-01-21 15:06:31 +0800

    Added Remove method for screening in IScreeningWriter interface

--- Core/Repository/IScreening.cs
+++ Core/Repository/IScreening.cs
@@ -14,6 +14,7 @@ namespace Core.Repository
     public interface IScreeningWriter
     {
         public void Add(Screening screening);
+        public void Remove(Screening screening);
         public void AddTicket(Ticket ticket);
     }
 

2de3a0cd8d82802588c23fb1fa3b22e77a33955e
Qin Guan <qinguan20040914@gmail.com>
2022-01-27 10:53:30 +0800

    Format

--- Core/Models/Cinema.cs
+++ Core/Models/Cinema.cs
@@ -5,8 +5,8 @@
         public string Name { get; set; }
         public int HallNo { get; set; }
         public int Capacity { get; set; }
-        
-        public static readonly string Header = $"{"Name",-15}{"Hall No",-10}{"Capacity", -10}";
+
+        public static readonly string Header = $"{"Name",-15}{"Hall No",-10}{"Capacity",-10}";
 
         public Cinema()
         {
@@ -21,7 +21,7 @@
 
         public override string ToString()
         {
-            return $"{Name,-15}{HallNo,-10}{Capacity, -10}";
+            return $"{Name,-15}{HallNo,-10}{Capacity,-10}";
         }
     }
 }
